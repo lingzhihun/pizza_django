a96c08b84a6c0063071a4cf8243b06fa469bff9e
lingzhihun <dai9313@gmail.com>
2023-02-16 18:05:26 +0800

    完成首页编写，还需要写其它页面的编写

--- pizzeria/pizzerias/urls.py
+++ pizzeria/pizzerias/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name = 'pizzerias'
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
+    re_path(r'^pizza/$',views.pizza,name='pizza'),
+    re_path(r'^pizza/?P<pizze_id>/d+$',views.Toppings,name='toppings')
 ]
--- pizzeria/pizzerias/views.py
+++ pizzeria/pizzerias/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Pizza, Toppings
 # Create your views here.
 def index(request):
     return render(request, 'pizzerias/index.html')
+
+def pizza(request):
+    pizzas = Pizza.objects.order_by("name")
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzerias/pizza.html', context)
